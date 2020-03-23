a7b2b5c74351aaab6ebd96798b750989c33c6f92
tony-goss <anthony.goss@tufts.edu>
2020-03-23 10:27:06 -0700

    form

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('ppl', views.alum_list, name='alum_list')
+    path('', views.home, name='home'),
+    path('ppl', views.alum_list, name='alum_list'),
+    path('post/new/', views.alumni_new, name='alumni_new')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post, Alumni
-
+from .forms import AlumniForm
 
 
 # Create your views here.
@@ -10,5 +10,20 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def alum_list(request):
-	people = Alumni.objects.all()
-	return render(request, 'blog/alum_list.html', {'people': people})
+    people = Alumni.objects.all()
+    return render(request, 'blog/alum_list.html', {'people': people})
+
+def alumni_new(request):
+    if request.method == 'POST':
+        form = AlumniForm(request.POST)
+        if form.is_valid():
+            person = form.save(commit=False)
+            person.date = timezone.now()
+            person.save()
+    else:
+        form = AlumniForm()
+    return render(request, 'blog/alum_edit.html', {'form': form})
+
+def home(request):
+    return render(request, 'blog/home.html')
+
